c259fda2d5580c20f2491a548f0b2c58b50be1bc
kndwin <tnkevino@gmail.com>
2022-10-27 17:03:31 +1100

    🔨: ignore type error

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,5 +2,6 @@ import "global.css";
 import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  // @ts-ignore
   return <Component {...pageProps} />;
 }
